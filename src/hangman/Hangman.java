0481c79309707da4e5b1de7e0247de652ab542bb
samir-matin <samir.matin.586@my.csun.edu>
2018-03-24 18:51:43 -0700

    Added Icon and Title

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Hangman extends Application {
@@ -17,6 +18,9 @@ public class Hangman extends Application {
         Parent root = loader.load();
         Scene scene = new Scene(root, 600, 800);
         scene.getStylesheets().add(getClass().getResource("Hangman.css").toExternalForm());
+
+        primaryStage.getIcons().add(new Image("file:images/peach.png"));
+        primaryStage.setTitle("Hangman");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
